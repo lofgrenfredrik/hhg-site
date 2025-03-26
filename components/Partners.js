2f9b4c70c0bdde2d5107bf8732a920bdedc1cd52
Fredrik Löfgren <fr3drik.l@gmail.com>
2025-03-26 09:28:56 +0000

    style: partners fixup

--- components/Partners.js
+++ components/Partners.js
@@ -4,9 +4,12 @@ export default function Partners({ partners }) {
   return (
     <section className="mx-auto w-full max-w-maximus px-3 py-8 text-zinc-50  md:p-8">
       <h2 className="font-heading text-3xl font-bold">{partners.title}</h2>
-      <div className="grid grid-cols-partners gap-5 pt-8 md:grid-cols-partners-lg">
+      <div className="grid grid-cols-partners gap-5 pt-8 md:grid-cols-partners-md lg:grid-cols-partners-lg">
         {partners.partnerLogos.map((partner) => (
-          <div key={partner.id} className="flex items-center rounded-lg bg-white p-4">
+          <div
+            key={partner.id}
+            className="flex items-center justify-center rounded-lg bg-white p-4"
+          >
             <Image
               src={partner.responsiveImage.src}
               alt={partner.responsiveImage.title}
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,8 @@ module.exports = {
         heading: ["var(--font-unna)"],
       },
       gridTemplateColumns: {
-        partners: "repeat(auto-fill, minmax(140px, 1fr))",
+        partners: "repeat(auto-fill, minmax(160px, 1fr))",
+        "partners-md": "repeat(auto-fill, minmax(120px, 1fr))",
         "partners-lg": "repeat(auto-fill, minmax(180px, 1fr))",
       },
     },
