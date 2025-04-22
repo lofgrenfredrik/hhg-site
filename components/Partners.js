2cda29f5dd36143466acbd0f0fced2d57fa04a65
Fredrik Löfgren <fr3drik.l@gmail.com>
2025-04-22 13:38:42 +0200

    feat: fix tailwind compability

--- components/Partners.js
+++ components/Partners.js
@@ -4,7 +4,7 @@ export default function Partners({ partners }) {
   return (
     <section className="mx-auto w-full max-w-maximus px-3 py-8 text-zinc-50  md:p-8">
       <h2 className="font-heading text-3xl font-bold">{partners.title}</h2>
-      <div className="grid grid-cols-partners gap-5 pt-8 md:grid-cols-partners-md lg:grid-cols-partners-lg">
+      <div className="grid partners gap-5 pt-8 md:partners-md lg:partners-lg">
         {partners.partnerLogos.map((partner) => (
           <div
             key={partner.id}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#E0A500",
-        primaryLighter: "#ffc82e",
-      },
-      maxWidth: { maximus: "1200px" },
-      fontFamily: {
-        heading: ["var(--font-unna)"],
-      },
-      gridTemplateColumns: {
-        partners: "repeat(auto-fill, minmax(160px, 1fr))",
-        "partners-md": "repeat(auto-fill, minmax(120px, 1fr))",
-        "partners-lg": "repeat(auto-fill, minmax(180px, 1fr))",
-      },
-    },
-  },
   plugins: [],
 }
