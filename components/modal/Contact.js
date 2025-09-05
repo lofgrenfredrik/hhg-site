ce4727bcbaa9a1a8640133b337a3cf6e6bd0d285
Fredrik Löfgren <fr3drik.l@gmail.com>
2025-09-05 15:18:23 +0200

    feat: update modal code

--- components/modal/Contact.js
+++ components/modal/Contact.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { Dialog, Transition } from "@headlessui/react"
+import { Dialog, Transition, TransitionChild, DialogTitle, DialogPanel } from "@headlessui/react"
 import { XMarkIcon } from "@heroicons/react/24/outline"
 import { Fragment, useRef } from "react"
 
@@ -18,7 +18,7 @@ export default function ContactModal({ contactInfo }) {
   return (
     <Transition appear show={isContactModalOpen} as={Fragment}>
       <Dialog onClose={closeContactModal} initialFocus={emailRef}>
-        <Transition.Child
+        <TransitionChild
           as={Fragment}
           enter="ease-out duration-300"
           enterFrom="opacity-0"
@@ -28,10 +28,10 @@ export default function ContactModal({ contactInfo }) {
           leaveTo="opacity-0"
         >
           <div className="fixed inset-0 bg-black/40" aria-hidden="true" />
-        </Transition.Child>
+        </TransitionChild>
 
         <div className="fixed inset-0 flex items-center justify-center lg:p-4 z-50">
-          <Transition.Child
+          <TransitionChild
             as={Fragment}
             enter="ease-out duration-300"
             enterFrom="opacity-0 scale-95"
@@ -40,12 +40,12 @@ export default function ContactModal({ contactInfo }) {
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <Dialog.Panel className="relative mx-auto flex h-screen w-screen flex-col items-center justify-center bg-white p-10 text-center transition-all lg:h-auto lg:max-w-[450px] lg:items-start lg:rounded-lg lg:text-left">
+            <DialogPanel className="relative mx-auto flex h-screen w-screen flex-col items-center justify-center bg-white p-10 text-center transition-all lg:h-auto lg:max-w-[450px] lg:items-start lg:rounded-lg lg:text-left">
               <button className="absolute top-5 right-4" onClick={closeContactModal} type="button">
                 <span className="sr-only">Close contact dialog</span>
                 <XMarkIcon className="h-6 w-6 text-black" />
               </button>
-              <Dialog.Title className="font-heading text-3xl font-bold">Contact</Dialog.Title>
+              <DialogTitle className="font-heading text-3xl font-bold">Contact</DialogTitle>
               <div
                 className="[&>p]:mt-3 [&>p]:text-base [&>*>em]:italic [&>*>strong]:font-bold"
                 dangerouslySetInnerHTML={{ __html: contactMessage }}
@@ -97,8 +97,8 @@ export default function ContactModal({ contactInfo }) {
                   <LinkedInIcon className="h-full w-full max-w-[50px]" />
                 </a>
               </div>
-            </Dialog.Panel>
-          </Transition.Child>
+            </DialogPanel>
+          </TransitionChild>
         </div>
       </Dialog>
     </Transition>
